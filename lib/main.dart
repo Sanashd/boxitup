616faf808711678f4eed1c2b2f8f315df1bb744d
Sanashd <sanashd20@gmail.com>
2023-06-05 11:58:33 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:boxitup/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        themeMode: ThemeMode.light,
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          // fontFamily: GoogleFonts.lato().fontFamily
+        ),
+        debugShowCheckedModeBanner: false,
+        // darkTheme: ThemeData(primarySwatch: Colors.green
+        routes: {
+          "/": (context) => const HomePage(),
+        });
+  }
+}
